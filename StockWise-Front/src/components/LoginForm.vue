389676075868220c46355b5e64329c61a3ff5cce
Pedro <40220243@esmad.ipp.pt>
2024-11-07 12:32:47 +0000

    Refactor project structure and remove unused components and styles

--- StockWise-Front/src/components/LoginForm.vue
+++ StockWise-Front/src/components/LoginForm.vue
@@ -8,32 +8,10 @@
       ></v-text-field>
   
       <v-text-field
-        v-model="phone.value.value"
-        :counter="7"
-        :error-messages="phone.errorMessage.value"
-        label="Phone Number"
-      ></v-text-field>
-  
-      <v-text-field
         v-model="email.value.value"
         :error-messages="email.errorMessage.value"
         label="E-mail"
       ></v-text-field>
-  
-      <v-select
-        v-model="select.value.value"
-        :error-messages="select.errorMessage.value"
-        :items="items"
-        label="Select"
-      ></v-select>
-  
-      <v-checkbox
-        v-model="checkbox.value.value"
-        :error-messages="checkbox.errorMessage.value"
-        label="Option"
-        type="checkbox"
-        value="1"
-      ></v-checkbox>
   
       <v-btn
         class="me-4"
